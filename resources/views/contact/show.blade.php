ad3bb046459e3b243b4e482fcde2fe1c579db01d
eprou <proutiereethan@gmail.com>
2023-05-17 13:15:14 +0200

    Update - v1.10 - Added admin part

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -2,34 +2,50 @@
 
 @section('content')
 
-<aside id="fh5co-hero" class="js-fullheight">
-    <div class="flexslider js-fullheight">
-        <ul class="slides">
-           <li style="background-image: url(images/slide_2.jpg);">
-               <div class="overlay-gradient"></div>
-               <div class="container">
-                   <div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
-                       <div class="slider-text-inner">
-                           <h2>Contact</h2>
-                           <p class="fh5co-lead">Messages reçus</p>
-                       </div>
-                   </div>
-               </div>
-           </li>
-          </ul>
-      </div>
-</aside>
+<header id="fh5co-header" role="banner">
+    <div class="container">
+        <div class="header-inner">
+            <h1><a href="{{ route('generics.index') }}">Prima Vera</a></h1>
+            <nav role="navigation">
+                <ul>
+                    <li class="header-bar-text"><a href="{{ route('generics.index') }}">Accueil</a></li>
+                    <li class="header-bar-text"><a href="{{ route('project.index') }}">Nos projets</a></li>
+                    <li class="header-bar-text"><a href="{{ route('generics.about') }}">À propos</a></li>
+                    <li class="header-bar-text active"><a href="{{ route('contact.index') }}">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<div class="animate-box">
+    <div class="col-md-6 col-md-offset-3 mt-5 mb-4 text-center">
+        <h2>Contact personnel</h2>
+    </div>
+</div>
 
 <div class="fh5co-contact animate-box">
     <div class="container">
         @foreach ($contacts as $contact)
             <div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
-                <h4>{{ $contact->name }}</h4>
-                <p> {{ $contact->message }} </p>
+                <div class="card">
+                    <div class="card-header">
+                      {{ $contact->name }}
+                    </div>
+                    <div class="card-body">
+                      <blockquote class="blockquote mb-0">
+                        <p>{{ $contact->message }}</p>
+                        <footer class="blockquote-footer">{{ $contact->name }} | {{ $contact->email }}</footer>
+                      </blockquote>
+                    </div>
+                  </div>
             </div>	
         @endforeach
 
     </div>
 </div>
+
+
+
 
 @endsection
